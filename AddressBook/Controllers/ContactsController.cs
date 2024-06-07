69e1079e766d9c1319de91b5d0c82ac3753ffe31
shahbaznew <shahbazayub0@gmail.com>
2024-06-07 05:25:49 +0100

    Controller File updating to accomadate service layer and AllContacts method updated

--- AddressBook/Controllers/ContactsController.cs
+++ AddressBook/Controllers/ContactsController.cs
@@ -1,4 +1,5 @@
 ﻿using AddressBook.Models;
+using AddressBook.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,16 +10,12 @@ namespace AddressBook.Controllers
     [ApiController]
     public class ContactsController : ControllerBase
     {
-        private static List<Contact> contacts = new List<Contact>()
-        {
-            new Contact() { Id = 1, FirstName = "Barry", LastName = "Long", PhoneNumber = "07986543154", Address = "1 Main Street",},
-            new Contact() { Id = 2, FirstName = "Paul", LastName = "Liv", PhoneNumber = "07164823571", Address = "2 Main Street",},
-            new Contact() { Id = 3, FirstName = "Smith", LastName = "Hudson", PhoneNumber = "07496558217", Address = "3 Main Street",}
-        };
+       
         [HttpGet]
         public IActionResult AllContacts()
         {
-            return Ok(contacts);
+            var service = new ContactService();
+            return Ok(service.GetAllContacts());
         }
 
         [HttpGet("{id}")]
