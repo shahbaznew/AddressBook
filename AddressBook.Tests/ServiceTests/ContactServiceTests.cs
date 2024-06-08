805eed49d367bb3817c82d1d50f3ba8673c7e57f
shahbaznew <shahbazayub0@gmail.com>
2024-06-08 08:34:09 +0100

    Creating Service Tests files and ensuring the test file reset before each test

---
+++ AddressBook.Tests/ServiceTests/ContactServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Tests.ServiceTests
+{
+    public class ContactServiceTests
+    {
+         private readonly string _testFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "test_contacts.json");
+
+        public ContactServiceTests()
+        {
+            if (File.Exists(_testFilePath))
+            {
+                File.Delete(_testFilePath);
+            }
+            File.WriteAllText(_testFilePath, "[]");
+        }
+    }
+}
